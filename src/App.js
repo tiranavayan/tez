eaa2fc4eab97cbb63ffad80f79490e2b343828d0
tiran <tiranavayan95@gmail.com>
2021-05-01 17:02:41 +0400

    second changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import react, { useState } from 'react';
+import react, { useEffect, useState } from 'react';
 
 const App = () => {
 
@@ -12,10 +12,10 @@ const App = () => {
   const [securityLevel, setSecurityLevel] = useState('none');
   const [projectType, setProjectType] = useState('none');
 
-  const processorValues = [2, 4, 6, 8, 10, 12, 16];
-  const ramValues = [8, 10, 12, 16, 32, 64];
-  const hddValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000];
-  const adaptorsCountValues = [1, 2, 3, 4, 5, 6];
+  const processorValues = [2, 4, 6, 8, 10, 12, 16, 20];
+  const ramValues = [8, 10, 12, 16, 32, 64, 128, 256];
+  const hddValues = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 6000, 10000];
+  const adaptorsCountValues = [1, 2, 3, 4, 5, 6, 8, 10];
 
   const securityLevelValues = [
     { value: 1, name: 'ցածր' },
@@ -24,7 +24,6 @@ const App = () => {
     { value: 4, name: 'միջին-բարձր' },
     { value: 5, name: 'բարձր' }
   ];
-
   const projectTypeValues = [
     { value: 1, name: 'ազատ բնույթի, կարճաժամկետ աշխատանքներ' },
     { value: 2, name: 'ուսանողական նախագծեր, ավարտական աշխատանքներ' },
@@ -34,131 +33,186 @@ const App = () => {
     { value: 6, name: 'գիտահետազտական բնույթի աշխատանքներ' }
   ];
 
+  const [cloud, setCloud] = useState('none');
+
+  const exitingClouds = [
+    { key: 1, name: 'Առաջին լոկալ հիբրիդային ամպ', p: 4, r: 16, v: 500, a: 1, s: 1, n: 1 },
+    { key: 2, name: 'Երկրորդ լոկալ հիբրիդային ամպ', p: 8, r: 16, v: 2000, a: 2, s: 2, n: 3 },
+    { key: 3, name: 'Առաջին գլոբալ հիբրիդային ամպ', p: 16, r: 128, v: 5000, a: 6, s: 5, n: 6 },
+    { key: 4, name: 'Երկրորդ գլոբալ հիբրիդային ամպ', p: 32, r: 256, v: 10000, a: 10, s: 3, n: 4 },
+  ];
+
+  const [selectedCloud, setSelectedCloud] = useState(null);
+
+  useEffect(() => {
+    setSelectedCloud(exitingClouds.find(obj => {
+      return obj.key == cloud
+    }));
+  }, [cloud]);
+
   const calculate = () => {
     if (
-      processor !== 'none'
-      && ram !== 'none'
-      && hdd !== 'none'
-      && adaptorsCount !== 'none'
-      && securityLevel !== 'none'
-      && projectType !== 'none'
+      processor === 'none'
+      || ram === 'none'
+      || hdd === 'none'
+      || adaptorsCount === 'none'
+      || securityLevel === 'none'
+      || projectType === 'none'
     ) {
-      setResult(`${processor} ${ram} ${hdd} ${adaptorsCount} ${securityLevel} ${projectType}`);
-    } else {
       setResult(`Ընտրեք բոլոր պարամետրերը`);
+    } else if (!selectedCloud) {
+      setResult(`Ընտրեք Հիբրիդային ամպը`);
+    } else  {
+      setResult(`${processor} ${ram} ${hdd} ${adaptorsCount} ${securityLevel} ${projectType}`);
     }
   };
 
   return (
     <div className="App">
-      <div className="title">Հիբրիդային ամպային համակարգում նախագծերի օպտիմալ բաշխումը Տիգրան Հոպար</div>
-      <div className="Container">
-        <div className="SelectWrapper">
-          <span className="SlectLabel">Պրոցեսորների քանակ (P)</span>
-          <select
-            name="processor"
-            className="Processor"
-            value={processor}
-            defaultValue={'none'}
-            onChange={(e) => setProcessor(e.target.value)}
-          >
-            <option className="Options" value="none" disabled>none</option>
-            {processorValues.map((value) => (
-              <option className="Options" key={`${value} ${Math.random()}`} value={value}>
-                {value} միջուկային
-              </option>
-            ))}
-          </select>
+      <div className="title">Հիբրիդային ամպային համակարգում նախագծերի օպտիմալ բաշխումը</div>
+      <div className="ContainerWrapper">
+        <div className="Container1">
+          <div className="smallTitle">Ցանկալի ամպային համակարգի տվյալներ</div>
+          <div className="SelectWrapper">
+            <span className="SlectLabel">Պրոցեսորների քանակ (P)</span>
+            <select
+              name="processor"
+              className="select"
+              value={processor}
+              defaultValue={'none'}
+              onChange={(e) => setProcessor(e.target.value)}
+            >
+              <option className="Options" value="none" disabled>none</option>
+              {processorValues.map((value) => (
+                <option className="Options" key={`${value} ${Math.random()}`} value={value}>
+                  {value} միջուկային
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="SelectWrapper">
+            <span className="SlectLabel">RAM-ի ծավալ (R)</span>
+            <select
+              name="ram"
+              className="select"
+              value={ram}
+              defaultValue={'none'}
+              onChange={(e) => setRam(e.target.value)}
+            >
+              <option className="Options" value="none" disabled>none</option>
+              {ramValues.map((value) => (
+                <option className="Options" key={`${value} ${Math.random()}`} value={value}>
+                  {value} գիգաբայթ
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="SelectWrapper">
+            <span className="SlectLabel">Հիշողության ծավալ (V)</span>
+            <select
+              name="hdd"
+              className="select"
+              value={hdd}
+              defaultValue={'none'}
+              onChange={(e) => setHdd(e.target.value)}
+            >
+              <option className="Options" value="none" disabled>none</option>
+              {hddValues.map((value) => (
+                <option className="Options" key={`${value} ${Math.random()}`} value={value}>
+                  {value >= 1000 ? value / 1000 : value} 
+                  {value >= 1000 ? ' Տերաբայթ' : ' Գիգաբայթ'}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="SelectWrapper">
+            <span className="SlectLabel">Ցանցային ադապտորների քանակ (A)</span>
+            <select
+              name="adaptorsCount"
+              className="select"
+              value={adaptorsCount}
+              defaultValue={'none'}
+              onChange={(e) => setAdaptorsCount(e.target.value)}
+            >
+              <option className="Options" value="none" disabled>none</option>
+              {adaptorsCountValues.map((value) => (
+                <option className="Options" key={`${value} ${Math.random()}`} value={value}>
+                  {value} հատ
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="SelectWrapper">
+            <span className="SlectLabel">Անվտանգության ցուցանիշ (S)</span>
+            <select
+              name="securityLevel"
+              className="select"
+              value={securityLevel}
+              defaultValue={'none'}
+              onChange={(e) => setSecurityLevel(e.target.value)}
+            >
+              <option className="Options" value="none" disabled>none</option>
+              {securityLevelValues.map((value) => (
+                <option className="Options" key={`${value} ${Math.random()}`} value={value.value}>
+                  {value.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="SelectWrapper">
+            <span className="SlectLabel">Նախագծի տեսակը (N)</span>
+            <select
+              name="projectType"
+              className="select"
+              value={projectType}
+              defaultValue={'none'}
+              onChange={(e) => setProjectType(e.target.value)}
+            >
+              <option className="Options" value="none" disabled>none</option>
+              {projectTypeValues.map((value) => (
+                <option className="Options" key={`${value} ${Math.random()}`} value={value.value}>
+                  {value.name}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-        <div className="SelectWrapper">
-          <span className="SlectLabel">RAM-ի ծավալ (R)</span>
-          <select
-            name="processor"
-            className="Processor"
-            value={ram}
-            defaultValue={'none'}
-            onChange={(e) => setRam(e.target.value)}
-          >
-            <option className="Options" value="none" disabled>none</option>
-            {ramValues.map((value) => (
-              <option className="Options" key={`${value} ${Math.random()}`} value={value}>
-                {value} գիգաբայթ
-              </option>
-            ))}
-          </select>
+        <div className="Container2">
+          <div className="smallTitle">Առկա ամպային համակարգեր</div>
+          <div className="SelectWrapper">
+            <span className="SlectLabel">Ընտրել</span>
+            <select
+              name="exitingClouds"
+              className="selectCloud"
+              value={cloud}
+              defaultValue={'none'}
+              onChange={(e) => setCloud(e.target.value)}
+            >
+              <option className="Options" value="none" disabled>none</option>
+              {exitingClouds.map((value) => (
+                <option className="Options" key={`${value.name}`} value={value.key}>
+                  {value.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          {selectedCloud ? (
+            <div className="parametersWrapper">
+              <div className="parameters">{`P = ${selectedCloud?.p} միջուկային`}</div>
+              <div className="parameters">{`R = ${selectedCloud?.r} գիգաբայթ`}</div>
+              <div className="parameters">{`V = ${selectedCloud?.v} գիգաբայթ`}</div>
+              <div className="parameters">{`A = ${selectedCloud?.a} հատ`}</div>
+              <div className="parameters">{`S = ${selectedCloud?.s}`}</div>
+              <div className="parameters">{`N = ${selectedCloud?.n}`}</div>
+            </div>
+          ) : null}
         </div>
-        <div className="SelectWrapper">
-          <span className="SlectLabel">Հիշողության ծավալ (V)</span>
-          <select
-            name="processor"
-            className="Processor"
-            value={hdd}
-            defaultValue={'none'}
-            onChange={(e) => setHdd(e.target.value)}
-          >
-            <option className="Options" value="none" disabled>none</option>
-            {hddValues.map((value) => (
-              <option className="Options" key={`${value} ${Math.random()}`} value={value}>
-                {value} գիգաբայթ
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="SelectWrapper">
-          <span className="SlectLabel">Ցանցային ադապտորների քանակ (A)</span>
-          <select
-            name="processor"
-            className="Processor"
-            value={adaptorsCount}
-            defaultValue={'none'}
-            onChange={(e) => setAdaptorsCount(e.target.value)}
-          >
-            <option className="Options" value="none" disabled>none</option>
-            {adaptorsCountValues.map((value) => (
-              <option className="Options" key={`${value} ${Math.random()}`} value={value}>
-                {value} հատ
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="SelectWrapper">
-          <span className="SlectLabel">Անվտանգության ցուցանիշ (S)</span>
-          <select
-            name="processor"
-            className="Processor"
-            value={securityLevel}
-            defaultValue={'none'}
-            onChange={(e) => setSecurityLevel(e.target.value)}
-          >
-            <option className="Options" value="none" disabled>none</option>
-            {securityLevelValues.map((value) => (
-              <option className="Options" key={`${value} ${Math.random()}`} value={value.value}>
-                {value.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="SelectWrapper">
-          <span className="SlectLabel">Նախագծի տեսակը (N)</span>
-          <select
-            name="processor"
-            className="Processor"
-            value={projectType}
-            defaultValue={'none'}
-            onChange={(e) => setProjectType(e.target.value)}
-          >
-            <option className="Options" value="none" disabled>none</option>
-            {projectTypeValues.map((value) => (
-              <option className="Options" key={`${value} ${Math.random()}`} value={value.value}>
-                {value.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button className="button" onClick={calculate}>Հաստատել</button>
-        {result && (
-          <div className="result">{result}</div>
-        )}
+      </div>
+      <div className="resultWrapper">
+          <button className="button" onClick={calculate}>Հաստատել</button>
+          {result && (
+            <div className="result">{result}</div>
+          )}
       </div>
     </div>
   );
